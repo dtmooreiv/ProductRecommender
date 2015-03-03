2a00b216a68feef9238263d1d6dfd479407c57ee
Philippe Bailer <pbailer@ncsu.edu>
2015-03-03 10:45:39 -0500

    Changed application.js to process an Arraylist of Recommendations rather then a recommendation object.

--- src/main/resources/assets/application.js
+++ src/main/resources/assets/application.js
@@ -1,28 +1,28 @@
 function getRecommendations() {
+    //Get user input for desired recommendations
 	var siteId = document.getElementById("siteIdInput").value;
 	var contactId = document.getElementById("contactIdInput").value;
 	var count = document.getElementById("countInput").value;
 	//We need to validate that these are all numbers
 
+    //Get the recommendations based on user input
 	var response = httpGet("/api/recommend/" + siteId + "/" + contactId + "?count=" + count);
-	var results = JSON.parse(response).productIds;
+	var recommendations = JSON.parse(response);
+
+	//Get and Remove all previous results
 	var orderedList = document.getElementById("results");
-	
-	//Remove all previous results.
-	var index;
 	var length = orderedList.children.length;
-	for(index = 0; index < length; index++) {
+	for(i = 0; i < length; i++) {
 		orderedList.children[0].parentNode.removeChild(orderedList.children[0]);
 	}
 
-	//Add a list item containing the productId and score to the results ordered list
-	results.forEach(function(_result) {
-		var resultElement = document.createElement("li");
-		var result = JSON.parse(_result);
-		var resultText = document.createTextNode(result.productId + " " + result.score);
-		resultElement.appendChild(resultText);
-		orderedList.appendChild(resultElement);	
-	});
+    //Add a list item containing the productId and score to the results ordered list for display
+    for(var i = 0; i < recommendations.length; i++) {
+        var resultElement = document.createElement("li");
+        var resultText = document.createTextNode(recommendations[i].productId + " " + recommendations[i].score);
+        resultElement.appendChild(resultText);
+        orderedList.appendChild(resultElement);
+    }
 }
 
 //This function sends a GET request to theUrl and returns the response text of the GET request.
