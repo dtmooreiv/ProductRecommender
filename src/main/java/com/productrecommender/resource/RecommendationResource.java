4902d4536c12edb1b82254652ca390f835fca22c
Philippe Bailer <pbailer@ncsu.edu>
2015-03-03 10:43:48 -0500

    Changed RecommendationResource to return an Arraylist of Recommendations as a JSON object.

--- src/main/java/com/productrecommender/resource/RecommendationResource.java
+++ src/main/java/com/productrecommender/resource/RecommendationResource.java
@@ -5,14 +5,8 @@ import com.productrecommender.services.scheduled.Preprocessor;
 import io.dropwizard.jersey.params.IntParam;
 import io.dropwizard.jersey.params.LongParam;
 import org.apache.mahout.cf.taste.common.TasteException;
-import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.recommender.CachingRecommender;
-import org.apache.mahout.cf.taste.impl.recommender.GenericBooleanPrefItemBasedRecommender;
-import org.apache.mahout.cf.taste.impl.similarity.LogLikelihoodSimilarity;
-import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
-import org.apache.mahout.cf.taste.recommender.Recommender;
-import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
@@ -25,8 +19,6 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -46,31 +38,35 @@ public class RecommendationResource {
     }
 
     @GET
-    public Recommendation recommend(@PathParam("siteId") LongParam siteId,
-                                    @PathParam("contactId") LongParam contactId,
-                                    @QueryParam("count") @DefaultValue("10")IntParam count) {
+    public ArrayList<Recommendation> recommend(@PathParam("siteId") LongParam siteId,
+                                               @PathParam("contactId") LongParam contactId,
+                                               @QueryParam("count") @DefaultValue("10")IntParam count) {
+
         logger.info(count + " recommendations requested for site id " + siteId + " contact id "  + contactId);
 
         Jedis conn = pool.getResource();
-        ArrayList<String> products = new ArrayList<>();
+        ArrayList<Recommendation> recommendations = new ArrayList<>();
 
         try {
             //Check to see if we have data for this site
             if(!conn.sismember(Preprocessor.site_set, siteId.toString())) {
-                return new Recommendation(products);
+                return recommendations;
             }
 
+            //Get list of recommendations
             List<RecommendedItem> recommendedItems = recommenders.get(siteId.get()).recommend(contactId.get(), count.get());
-            for(RecommendedItem item: recommendedItems) {
-                products.add("{\"productId\": \"" + conn.hget(Preprocessor.productCatalog + siteId, item.getItemID() + "") + "\", " + " \"score\": \"" + item.getValue() + "\"}");
-            }
 
+            //Turn list of recommendations into a JSON arraylist
+            for(RecommendedItem item: recommendedItems) {
+                String productId = conn.hget(Preprocessor.productCatalog + siteId, Long.toString(item.getItemID()));
+                String score = Float.toString(item.getValue());
+                recommendations.add(new Recommendation(productId, score));
+            }
         } catch (TasteException e) {
             e.printStackTrace();
         }
 
         pool.returnResource(conn);
-        return new Recommendation(products);
+        return recommendations;
     }
-
 }
