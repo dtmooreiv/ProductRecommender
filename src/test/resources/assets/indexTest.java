515b5af8c35e773549c7f0ffff357825f2fec52b
michael <mehubbar@ncsu.edu>
2015-04-04 19:59:09 -0400

    Introduced Selenium test file for index.html and relevant dependencies in pom.xml.  No tests implemented yet.

---
+++ src/test/resources/assets/indexTest.java
@@ -0,0 +1,11 @@
+package assets;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.Selenium;
+import com.thoughtworks.selenium.SeleniumException;
+
+public class indexTest {
+
+
+
+}
